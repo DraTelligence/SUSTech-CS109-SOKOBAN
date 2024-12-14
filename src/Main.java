e5d0f8f17a1c868a297b4f9d63245bc8b88f14ab
lyf <12412851@mail.sustech.edu.cn>
2024-12-14 10:54:30 +0800

    gengxing2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
-import javax.swing.*;
-import view.level.LevelFrame;
-import view.login.LoginFrame;
-
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 public class Main {
     public static void main(String[] args) {
-            LoginFrame loginFrame = new LoginFrame(500,300);
-            loginFrame.setVisible(true);
+        //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
+        // 查看 IntelliJ IDEA 建议如何修正。
+        System.out.printf("Hello and welcome!");
 
-            LevelFrame levelFrame = new LevelFrame(500,300);
-            levelFrame.setVisible(false);
-            loginFrame.setLevelFrame(levelFrame);
-
-
+        for (int i = 1; i <= 5; i++) {
+            //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
+            // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
+            System.out.println("i = " + i);
+        }
     }
 }
